466803155e8af17ca4fb52a8b8225b60b28bc155
sachakborameiy <borameiy.vs@gmail.com>
2024-06-03 15:54:51 +0700

    fixed: filter and all fields in table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,7 +134,7 @@ export default function MapClassification() {
           style={{ fontFamily: "MyFont, sans-serif", maxWidth: "30rem" }}
         >
           <div className="mt-2">
-            <div className=" rounded-lg  p-1 pl-4">
+            <div className=" rounded-lg p-0.5 pl-4">
               <div className="flex">
                 <div className="mt-2 w-[25%]">
                   <label className="block text-sm font-medium  text-gray-700">
@@ -144,7 +144,7 @@ export default function MapClassification() {
                 <div className="w-[75%] relative">
                   <div>
                     <select
-                      className="w-full p-2 mb-4 outline-none sm:text-sm border shadow-sm
+                      className="w-full p-2 mb-4 outline-none cursor-pointer sm:text-sm border shadow-sm
                         border-[#64d1ff] rounded-md appearance-none"
                       id="province_city"
                       name="province_city"
@@ -177,18 +177,18 @@ export default function MapClassification() {
               </div>
 
               <div className="flex">
-                <div className="mt-2 w-[25%]">
+                <div className="mt-1.5 w-[25%]">
                   <label className="block text-sm font-medium text-gray-700 pb-2">
                     ស្រុក/ក្រុង/ខណ្ខ
                   </label>
                 </div>
-                <div className="w-[75%] relative">
+                <div className="w-[75%] relative" style={{marginTop: "-0.5em"}}>
                   <div>
                     <select
                       onChange={(event) => {
                         setselectedDistrict(event.target.value);
                       }}
-                      className="w-full p-2 mb-4 outline-none sm:text-sm border shadow-sm border-[#64d1ff] rounded-md appearance-none"
+                      className="w-full p-2 mb-4 outline-none cursor-pointer sm:text-sm border shadow-sm border-[#64d1ff] rounded-md appearance-none"
                     >
                       <option
                         value=""
@@ -226,19 +226,19 @@ export default function MapClassification() {
               </div>
 
               <div className="flex">
-                <div className="mt-2 w-[25%]">
+                <div className="mt-1.5 w-[25%]">
                   <label className="block text-sm font-medium text-gray-700 pb-2">
                     ឃុំ/សង្កាត់
                   </label>
                 </div>
-                <div className="w-[75%] relative">
+                <div className="w-[75%] relative" style={{marginTop: "-0.5em"}}>
                   <div>
                     <select
                       onChange={(event) => {
                         setselecteCommue(event.target.value);
                         setShouldRunEffect(true);
                       }}
-                      className="w-full p-2 mb-4 outline-none sm:text-sm border shadow-sm border-[#64d1ff] rounded-md appearance-none"
+                      className="w-full p-2 mb-4 outline-none cursor-pointer sm:text-sm border shadow-sm border-[#64d1ff] rounded-md appearance-none"
                     >
                       {commues.length > 0 ? (
                         commues.map((province_city, index) => (
@@ -268,19 +268,19 @@ export default function MapClassification() {
               </div>
 
               <div className="flex">
-                <div className="mt-2 w-[25%]">
+                <div className="mt-1.5 w-[25%]">
                   <label className="block text-sm font-medium text-gray-700 pb-2">
                     ភូមិ
                   </label>
                 </div>
-                <div className="w-[75%] relative">
+                <div className="w-[75%] relative" style={{marginTop: "-0.5em"}}>
                   <div>
                     <select
                       onChange={(event) => {
                         setselecteVillage(event.target.value);
                         setShouldRunEffect(true);
                       }}
-                      className="w-full p-2 mb-4 outline-none sm:text-sm border shadow-sm border-[#64d1ff] rounded-md appearance-none"
+                      className="w-full p-2 mb-4 outline-none cursor-pointer sm:text-sm border shadow-sm border-[#64d1ff] rounded-md appearance-none"
                     >
                       {villages.length > 0 ? (
                         villages.map((province_city, index) => (
@@ -326,7 +326,7 @@ export default function MapClassification() {
         >
           <div className="mt-2 p-4">
             <div className="flex" style={{ width: "100%" }}>
-              <div className="w-2 h-2 bg-red-500 mt-2"></div>
+              <div className="w-2 h-2 mt-2"></div>
               <div className="ml-2">
                 <label className="block text-md font-extrabold text-gray-700">
                   ទីតាំង​ដែលបានជ្រើសរើស
@@ -334,41 +334,84 @@ export default function MapClassification() {
               </div>
             </div>
           </div>
-          <div
-           
-          >
-            <div className="absolute top-24 w-1/2 left-1/4 bg-white opacity-90 z-50" style={{ flex: "1 1 auto", overflow: "auto" }}>
-              <table style={{ width: "100%", tableLayout: "fixed" }}>
-                <thead>
-                  <tr>
-                    <th>ខេត្ត/រាជធានី</th>
-                    <th>កូដ ខេត្ត/រាជធានី</th>
-                    <th>សាខា</th>
-                    <th>កូដ សាខា</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {mapdata &&
-                    mapdata.map((item: any, index: number) => (
-                      <tr key={index}>
-                        <td>{item["ខេត្ត/រាជធានី"]}</td>
-                        <td>{item["កូដ ខេត្ត/រាជធានី "]}</td>
-                        <td>{item["សាខា"]}</td>
-                        <td>{item["កូដ សាខា"]}</td>
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
+          <div>
+            <div className="flex items-center justify-center m-auto ">
+              {/* test */}
+              <div className="shadow-md rounded-lg overflow-scroll absolute top-7 bg-white opacity-90 z-50 border shadow-gray-400 " style={{ width: "85%" }}>
+                <table className="table-custom table-auto overflow-y-auto scrollbar-thin scrollbar-thumb-red-900 scrollbar-track-red-100" style={{ width: "100%", tableLayout: "fixed" }}>
+                  <thead className="bg-[#1B3351] text-white">
+                    <tr className="">
+                      <th className="font-normal border px-4 py-2 text-sm w-40">ខេត្ត/រាជធានី</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">កូដ ខេត្ត/រាជធានី</th>
+                      <th className="font-normal border px-4 py-2 text-sm" style={{width: "7.5em"}}>សាខា</th>
+                      <th className="font-normal border px-4 py-2 text-sm" style={{width: "7.5em"}}>កូដ សាខា</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">ស្រុក/ខណ្ឌ/ក្រុង</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">កូដ ស្រុក/ខណ្ឌ/ក្រុង</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">ឃុំ/សង្កាត់</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">កូដ ឃុំ/សង្កាត់</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">លេខទូរស័ព្ទ ឃុំ/សង្កាត់</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">លេខទូរស័ព្ទ មេភូមិ</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40" style={{width: "8.5em"}}>ចំនួន គ្រួសារ</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">L-Map (ភាគរយ)</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40" style={{width: "7em"}}>ចំណាត់ថ្នាក់</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40" style={{width: "95em"}}>កម្រិតទីតាំង</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">ដីពាណិជ្ជកម្ម C1</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">ដីពាណិជ្ជកម្ម C2</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">ដីពាណិជ្ជកម្ម C3</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">ដីលំនៅឋាន R1</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">ដីលំនៅឋាន R2</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">ដីលំនៅឋាន R3</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">ដីឧស្សាហកម្ម I1</th>
+                      <th className="font-normal border px-4 py-2 text-sm w-40">ដីឧស្សាហកម្ម I3</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {mapdata &&
+                      mapdata.map((item: any, index: number) => (
+                        <tr key={index} className="border-b hover:bg-[#284870] hover:text-white bg-gray-100 cursor-pointer">
+                          <td className="border px-4 py-2 text-sm">{item["ខេត្ត/រាជធានី"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["កូដ ខេត្ត/រាជធានី "]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["សាខា"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["កូដ សាខា"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["ស្រុក/ខណ្ឌ/ក្រុង"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["កូដ ស្រុក/ខណ្ឌ/ក្រុង"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["ឃុំ/សង្កាត់"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["កូដ ឃុំ/សង្កាត់"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["លេខទូរស័ព្ទ ឃុំ/សង្កាត់"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["លេខទូរស័ព្ទ មេភូមិ"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["ចំនួន គ្រួសារ"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["L-Map (ភាគរយ)"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["ចំណាត់ថ្នាក់"]}</td>
+                          <td className="border px-4 py-2 text-sm line-clamp-3">{item["កម្រិតទីតាំង"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["ចំណាត់ថ្នាក់"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["ដីពាណិជ្ជកម្ម C1"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["ដីពាណិជ្ជកម្ម C2 "]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["ដីពាណិជ្ជកម្ម C3"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["ដីលំនៅឋាន R1"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["ដីលំនៅឋាន R2"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["ដីលំនៅឋាន R3"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["ដីឧស្សាហកម្ម I1"]}</td>
+                          <td className="border px-4 py-2 text-sm">{item["ដីឧស្សាហកម្ម I2"]}</td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
-            <div style={{ flex: "1 1 auto", height: "55vh", position: "relative" }}>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1001033.0502020214!2d102.76085120509809!3d11.45931746038002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3105f033a39282f3%3A0x37e95344b24a2873!2sKoh%20Kong%20Province!5e0!3m2!1sen!2skh!4v1716522509201!5m2!1sen!2skh"
-                style={{ border: "0", width: "100%", height: "100%" }}
-                allowFullScreen
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-              ></iframe>
+
+            <div className="w-full h-screen flex flex-col">
+              <div className="flex-1 h-[40vh] relative">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m24!1m12!1m3!1d3908.7812834525052!2d104.8896271748692!3d11.567531138633484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m9!3e6!4m3!3m2!1d11.5675227!2d104.89225789999999!4m3!3m2!1d11.567407!2d104.89189309999999!5e0!3m2!1sen!2skh!4v1717375442940!5m2!1sen!2skh"
+                  className="border-0 w-full h-full"
+                  allowFullScreen
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                >
+                </iframe>              
+              </div>
             </div>
+
           </div>
         </div>
       </div>
